0b71eacf2c3962ac911254372465cedc5c849c86
tamtak8 <152185397+tamtak8@users.noreply.github.com>
2023-12-15 00:02:53 +0900

    Add files via upload

---
+++ app.js
@@ -0,0 +1,23 @@
+const container = document.querySelector(".container");
+const text = document.getElementById("text");
+
+let totalTime = 7500;
+let breatheTime = (totalTime / 5) * 2;
+let holdTime = totalTime / 5;
+
+meditateAnimation();
+
+function meditateAnimation() {
+    text.innerHTML = "吸って！";
+    container.className = "container grow";
+    setTimeout(() => {
+        text.innerHTML = "止めて！";
+
+        setTimeout(() => {
+            text.innerHTML = "吐いて！";
+            container.className = "container shrink";
+        }, holdTime);
+    }, breatheTime);
+}
+
+setInterval(meditateAnimation, totalTime);
